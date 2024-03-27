6a5d55b1b09f43bf7d2f3b72ff08420adab91ee7
Bryan31542 <00063820@uca.edu.sv>
2024-03-27 01:02:38 -0600

    :sparkles: Feat: delete location

--- src/controllers/location.js
+++ src/controllers/location.js
@@ -1,71 +1,71 @@
-const { response } = require("express");
-const { Location } = require("../models");
+const { response } = require('express')
+const { Location, Route } = require('../models')
 
 const locationGet = async (req, res = response) => {
-  const { limit = 5, skip = 0 } = req.query;
-  const query = {};
+  const { limit = 5, skip = 0 } = req.query
+  const query = {}
 
   const [total, locations] = await Promise.all([
     Location.countDocuments(query),
-    Location.find(query).limit(Number(limit)).skip(Number(skip)),
-  ]);
+    Location.find(query).limit(Number(limit)).skip(Number(skip))
+  ])
 
   res.json({
     total,
-    locations,
-  });
-};
+    locations
+  })
+}
 
 const locationGetOne = async (req, res = response) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  const location = await Location.findById(id);
+  const location = await Location.findById(id)
 
   res.json({
-    location,
-  });
-};
+    location
+  })
+}
 
 const locationPost = async (req, res = response) => {
-  const { name, description, latitude, longitude } = req.body;
-  const image = req.file.filename;
+  const { name, description, latitude, longitude } = req.body
+  const image = req.file.filename
 
   const location = new Location({
     name,
     description,
     image,
     latitude,
-    longitude,
-  });
+    longitude
+  })
 
-  await location.save();
+  await location.save()
 
   res.json({
-    location,
-  });
-};
+    location
+  })
+}
 
 const locationPut = async (req, res = response) => {
-  const { id } = req.params;
-  const { ...data } = req.body;
+  const { id } = req.params
+  const { ...data } = req.body
 
-  let image;
+  let image
 
   if (req.file) {
-    image = req.file.filename;
+    image = req.file.filename
   }
 
   try {
-    let updatedLocation;
+    let updatedLocation
 
     if (image) {
       // remove old image
-      const location = await Location.findById(id);
+      const location = await Location.findById(id)
       if (location.image) {
-        const pathImage = `./uploads/${location.image}`;
+        const pathImage = `./uploads/${location.image}`
 
         if (fs.existsSync(pathImage)) {
-          fs.unlinkSync(pathImage);
+          fs.unlinkSync(pathImage)
         }
       }
 
@@ -73,28 +73,58 @@ const locationPut = async (req, res = response) => {
         id,
         { ...data, image }, // Include image data if present
         { new: true }
-      );
+      )
     } else {
       updatedLocation = await Location.findByIdAndUpdate(
         id,
         data, // Update only the data if no image
         { new: true }
-      );
+      )
     }
 
     res.json({
-      updatedLocation,
-    });
+      updatedLocation
+    })
   } catch (error) {
     return res.status(500).json({
-      msg: "Internal server error",
-    });
+      msg: 'Internal server error'
+    })
   }
-};
+}
+
+const locationDelete = async (req, res = response) => {
+  const { id } = req.params
+
+  const location = await Location.findById(id)
+
+  if (location.image) {
+    const pathImage = `./uploads/${location.image}`
+
+    if (fs.existsSync(pathImage)) {
+      fs.unlinkSync(pathImage)
+    }
+  }
+
+  // check if the location is in any route
+  const isUsed = await Route.findOne({ locations: id })
+
+  if (isUsed) {
+    return res.status(400).json({
+      msg: 'Location is in use'
+    })
+  }
+
+  await Location.findByIdAndDelete(id)
+
+  res.json({
+    msg: 'Location deleted'
+  })
+}
 
 module.exports = {
   locationGet,
   locationGetOne,
   locationPost,
   locationPut,
-};
+  locationDelete
+}
--- src/routes/location.js
+++ src/routes/location.js
@@ -1,58 +1,71 @@
-const { Router } = require("express");
+const { Router } = require('express')
 const {
   locationPost,
   locationGet,
   locationGetOne,
   locationPut,
-} = require("../controllers/location");
-const { validateJWT, validateFields, isAdminRole } = require("../middlewares");
-const { check } = require("express-validator");
-const { locationExistsById } = require("../helpers");
-const { upload } = require("../middlewares/multer-file");
+  locationDelete
+} = require('../controllers/location')
+const { validateJWT, validateFields, isAdminRole } = require('../middlewares')
+const { check } = require('express-validator')
+const { locationExistsById } = require('../helpers')
+const { upload } = require('../middlewares/multer-file')
 
-const router = Router();
+const router = Router()
 
 // Get All Locations
-router.get("/", [validateJWT, validateFields], locationGet);
+router.get('/', [validateJWT, validateFields], locationGet)
 
 // Get One Location
 router.get(
-  "/:id",
+  '/:id',
   [
-    check("id", "Invalid id").isMongoId(),
-    check("id").custom(locationExistsById),
+    check('id', 'Invalid id').isMongoId(),
+    check('id').custom(locationExistsById),
     validateJWT,
-    validateFields,
+    validateFields
   ],
   locationGetOne
-);
+)
 
 // Create Location
 router.post(
-  "/",
+  '/',
   [
     validateJWT,
-    upload.single("image"),
-    check("name", "name is required").not().isEmpty(),
-    check("latitude", "latitude is required").not().isEmpty(),
-    check("longitude", "longitude is required").not().isEmpty(),
+    upload.single('image'),
+    check('name', 'name is required').not().isEmpty(),
+    check('latitude', 'latitude is required').not().isEmpty(),
+    check('longitude', 'longitude is required').not().isEmpty(),
     isAdminRole,
-    validateFields,
+    validateFields
   ],
   locationPost
-);
+)
 
 router.put(
-  "/:id",
+  '/:id',
   [
     validateJWT,
-    upload.single("image"),
-    check("id", "Invalid id").isMongoId(),
-    check("id").custom(locationExistsById),
+    upload.single('image'),
+    check('id', 'Invalid id').isMongoId(),
+    check('id').custom(locationExistsById),
     isAdminRole,
-    validateFields,
+    validateFields
   ],
   locationPut
-);
+)
 
-module.exports = router;
+router.delete(
+  '/:id',
+  [
+    check('id', 'Invalid id').isMongoId(),
+    check('id').custom(locationExistsById),
+    validateJWT,
+    isAdminRole,
+    validateFields
+  ],
+  locationDelete
+)
+
+module.exports = router
