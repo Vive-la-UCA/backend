3999bd7fd64ba2a2facaa9320f531a84a932e7b8
Bryan31542 <00063820@uca.edu.sv>
2024-04-19 14:20:17 -0600

    :sparkles: Feat: get users no pagination

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,51 +1,62 @@
-const { response, request } = require("express");
-const bcrypt = require("bcryptjs");
+const { response, request } = require('express')
+const bcrypt = require('bcryptjs')
 
-const { User } = require("../models");
+const { User } = require('../models')
 
 const usersGet = async (req = request, res = response) => {
-  const { limit = 5, skip = 0 } = req.query;
-  const query = { status: true };
+  const { limit = 5, skip = 0 } = req.query
+  const query = { status: true }
 
   const [total, users] = await Promise.all([
     User.countDocuments(query),
-    User.find(query).limit(Number(limit)).skip(Number(skip)),
-  ]);
+    User.find(query).limit(Number(limit)).skip(Number(skip))
+  ])
 
   res.json({
     total,
-    users,
-  });
-};
+    users
+  })
+}
+
+const usersGetNoPagination = async (req = request, res = response) => {
+  const query = { status: true }
+
+  const users = await User.find(query)
+
+  res.json({
+    users
+  })
+}
 
 const usersGetOne = async (req, res = response) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  const user = await User.findById(id);
+  const user = await User.findById(id)
 
   res.json({
-    user,
-  });
-};
+    user
+  })
+}
 
 const usersPost = async (req = request, res = response) => {
-  const { name, email, password } = req.body;
-  const user = new User({ name, email, password });
+  const { name, email, password } = req.body
+  const user = new User({ name, email, password })
 
   // Encrypt password
-  const salt = bcrypt.genSaltSync();
-  user.password = bcrypt.hashSync(password, salt);
+  const salt = bcrypt.genSaltSync()
+  user.password = bcrypt.hashSync(password, salt)
 
   // Save in database
-  await user.save();
+  await user.save()
 
   res.json({
-    user,
-  });
-};
+    user
+  })
+}
 
 module.exports = {
   usersPost,
   usersGetOne,
   usersGet,
-};
+  usersGetNoPagination
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,40 +1,48 @@
-const { Router } = require("express");
-const { usersPost, usersGet, usersGetOne } = require("../controllers/user");
-const { check } = require("express-validator");
-const { emailExists, userExistsById } = require("../helpers");
-const { validateFields, validateJWT } = require("../middlewares");
+const { Router } = require('express')
+const {
+  usersPost,
+  usersGet,
+  usersGetOne,
+  usersGetNoPagination
+} = require('../controllers/user')
+const { check } = require('express-validator')
+const { emailExists, userExistsById } = require('../helpers')
+const { validateFields, validateJWT } = require('../middlewares')
 
-const router = Router();
+const router = Router()
 
 // Get All Users
-router.get("/", [validateJWT, validateFields], usersGet);
+router.get('/', [validateJWT, validateFields], usersGet)
+
+// Get All Users without pagination
+router.get('/all', [validateJWT, validateFields], usersGetNoPagination)
 
 // Get One User
 router.get(
-  "/:id",
+  '/:id',
   [
-    check("id", "Invalid id").isMongoId(),
-    check("id").custom(userExistsById),
+    check('id', 'Invalid id').isMongoId(),
+    check('id').custom(userExistsById),
     validateJWT,
-    validateFields,
+    validateFields
   ],
   usersGetOne
-);
+)
 
 // Create User
 router.post(
-  "/",
+  '/',
   [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Email is required").isEmail(),
-    check("email").custom(emailExists),
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('email').custom(emailExists),
     check(
-      "password",
-      "Password is required and greater than 8 digits"
+      'password',
+      'Password is required and greater than 8 digits'
     ).isLength({ min: 8 }),
-    validateFields,
+    validateFields
   ],
   usersPost
-);
+)
 
-module.exports = router;
+module.exports = router
