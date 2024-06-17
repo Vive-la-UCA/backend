3304e0bb15a804240bf9dec5c59890c87ee13bed
Bryan31542 <00063820@uca.edu.sv>
2024-06-16 22:11:41 -0600

    :ambulance: Hotfix: duplicated emails on register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,38 +1,44 @@
-const { Router } = require("express");
-const { check } = require("express-validator");
-const { login, loginGoogle, checkToken } = require("../controllers/auth");
+const { Router } = require('express')
+const { check } = require('express-validator')
+const { login, loginGoogle, checkToken } = require('../controllers/auth')
 const {
   validateFields,
   checkBearerToken,
-  validateJWT,
-} = require("../middlewares");
-const { usersPost } = require("../controllers/user");
+  validateJWT
+} = require('../middlewares')
+const { usersPost } = require('../controllers/user')
+const { emailExists } = require('../helpers')
 
-const router = Router();
+const router = Router()
 
 router.post(
-  "/login",
+  '/login',
   [
-    check("email", "Enter a valid email").isEmail(),
-    check("password", "Password is required").not().isEmpty(),
-    validateFields,
+    check('email', 'Enter a valid email').isEmail(),
+    check('password', 'Password is required').not().isEmpty(),
+    validateFields
   ],
   login
-);
+)
 
 router.post(
-  "/register",
+  '/register',
   [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Enter a valid email").isEmail(),
-    check("password", "Password is required").not().isEmpty(),
-    validateFields,
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter a valid email').isEmail(),
+    check('email').custom(emailExists),
+    check('password', 'Password is required').not().isEmpty(),
+    check(
+      'password',
+      'Password is required and greater than 8 digits'
+    ).isLength({ min: 8 }),
+    validateFields
   ],
   usersPost
-);
+)
 
-router.post("/google", [checkBearerToken, validateFields], loginGoogle);
+router.post('/google', [checkBearerToken, validateFields], loginGoogle)
 
-router.get("/check-token", [validateJWT, validateFields], checkToken);
+router.get('/check-token', [validateJWT, validateFields], checkToken)
 
-module.exports = router;
+module.exports = router
